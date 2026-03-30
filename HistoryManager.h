da421a5ea8a7820125640071db65190527549940
elenagutierrez28 <elenalaura.gutierrez@gmail.com>
2026-03-29 20:41:21 -0500

    Add HistoryManager class with basic functionality

--- HistoryManager.h
+++ HistoryManager.h
@@ -1,1 +1,25 @@
+#ifndef HISTORY_MANAGER_H
+#define HISTORY_MANAGER_H
 
+#include <vector>
+#include <string>
+
+class HistoryManager {
+private:
+    std::vector<std::string> history;
+
+public:
+    // Constructor
+    HistoryManager();
+
+    // Add entry to history
+    void addEntry(const std::string& entry);
+
+    // Display history
+    void displayHistory() const;
+
+    // Clear history
+    void clearHistory();
+};
+
+#endif
