c5461dea57831742d6ae805dfcc09a266b2c840a
justingarstecki <jgarstecki0089@eagle.fgcu.edu>
2026-04-12 22:53:44 -0400

    Update ExpressionParser.cpp

--- ExpressionParser.cpp
+++ ExpressionParser.cpp
@@ -3,33 +3,31 @@
 #include <sstream>
 #include <stdexcept>
 
+void ExpressionParser::setExpression(const std::string& expr) {
+    expression = expr;
+}
+
 double ExpressionParser::evaluateExpression() {
     double a, b;
     char op;
 
     std::istringstream iss(expression);
 
-    // Validate input format
     if (!(iss >> a >> op >> b)) {
         throw std::runtime_error("Invalid expression format. Use: number operator number");
     }
 
-    // Use AdvancedCalculator so project shows full OOP structure
     AdvancedCalculator calc;
 
     switch (op) {
         case '+':
             return calc.add(a, b);
-
         case '-':
             return calc.subtract(a, b);
-
         case '*':
             return calc.multiply(a, b);
-
         case '/':
             return calc.divide(a, b);
-
         default:
             throw std::runtime_error("Invalid operator. Use +, -, *, or /");
     }
