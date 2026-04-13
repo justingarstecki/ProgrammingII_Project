6b84baefcb7abca72fac4c133a8d2761ea4a7748
justingarstecki <jgarstecki0089@eagle.fgcu.edu>
2026-04-12 22:13:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include "UserInterface.h"
 
-using namespace std;
-
 int main() {
-    // Create the user interface object
     UserInterface ui;
 
-    // Display the menu (skeleton placeholder for now)
     ui.displayMenu();
 
-    cout << "Advanced Command-Line Calculator initialized." << endl;
     return 0;
 }
