f868c4cf43f6b8041856408587009079935b96e6
justingarstecki <jgarstecki0089@eagle.fgcu.edu>
2026-04-12 22:03:25 -0400

    Change to historyList

--- HistoryManager.h
+++ HistoryManager.h
@@ -1,24 +1,23 @@
-#pragma once
+#ifndef HISTORYMANAGER_H
+#define HISTORYMANAGER_H
+
 #include <vector>
 #include <string>
 
 class HistoryManager {
 private:
-    std::vector<std::string> history;
+    std::vector<std::string> historyList;
 
 public:
-    // Constructor
     HistoryManager();
 
-    // Add entry to history
     void addEntry(const std::string& entry);
 
-    // Get all history enteries 
     std::vector<std::string> getHistory() const;
 
-    // Get number of enteries
     size_t getHistorySize() const;
 
-    // Clear history
     void clearHistory();
 };
+
+#endif
