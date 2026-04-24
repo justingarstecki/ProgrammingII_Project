51ea0bddccd0aca95a9b1b22c24a84073d1361b4
justingarstecki <jgarstecki0089@eagle.fgcu.edu>
2026-04-23 21:14:57 -0400

    Update AdvancedCalculator.h (override base functions)

--- AdvancedCalculator.h
+++ AdvancedCalculator.h
@@ -7,6 +7,13 @@
 
 class AdvancedCalculator : public Calculator {
 public:
+    // Override base class functions
+    double add(double a, double b) override;
+    double subtract(double a, double b) override;
+    double multiply(double a, double b) override;
+    double divide(double a, double b) override;
+
+    // Advanced functions
     double sine(double value);
     double cosine(double value);
     double logarithm(double value);
